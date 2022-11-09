bfa1f603525bf10cafd6ed70c36d4dfdbc7dd827
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-11-09 18:07:32 +0000

    add card structures and animations

--- scrips/script.js
+++ scrips/script.js
@@ -1,4 +1,20 @@
-let myLibrary = []; 
+let myLibrary = [
+        {
+            "id": 1,
+            "title": "Hobbit",
+            "author": "J J R Tolkien",
+        },
+        {
+            "id": 2,
+            "title": "White Noise",
+            "author": "Don Delilo",
+        },
+        {
+            "id": 3,
+            "title": "The Witcher",
+            "author": "Andrew Sapkowski",
+        }
+    ]
 
 function Book() {
     // the constructor...
