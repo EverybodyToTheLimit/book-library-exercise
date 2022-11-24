42cdfd10f5cd1ae90f1e4ee5dc4acfd9deb31e53
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-11-24 11:39:27 +0000

    Add new button functionality which constructs new object and pushes it into array

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,28 @@
-let currentId = 3; 
+let currentId = 2; 
 
 let myLibrary = [
         {
-            "id": 2,
+            "id": 0,
             "title": "Hobbit",
             "author": "J R R Tolkien",
             "img": "https://storage.googleapis.com/lr-assets/kids/covers/_hires_imported/9780261103252.jpg",
         },
         {
-            "id": 3,
+            "id": 1,
             "title": "White Noise",
             "author": "Don Delilo",
             "img": "https://d3525k1ryd2155.cloudfront.net/h/187/168/283168187.0.l.jpg",
         },
         {
-            "id": 4,
+            "id": 2,
             "title": "The Witcher",
             "author": "Andrew Sapkowski",
             "img": "https://www.orbitbooks.net/wp-content/uploads/2022/06/Sapkowski_TimeofContempt_HC-scaled.jpg",
         }
     ]
 
-function Book(title, author, img) {
+function Book(id, title, author, img) {
+    this.id = id
     this.title = title
     this.author = author
     this.img = img
@@ -34,38 +35,67 @@ function addBookToLibrary() {
     keys.forEach((key, index) => {
 
 
-        let tempId = myLibrary[key].id;
-        let tempTitle = myLibrary[key].title;
-        let tempAuthor = myLibrary[key].author;
-        let tempImg = myLibrary[key].img;
-
-        let cardDiv = document.createElement("div"); 
-        imageDiv = document.createElement("div");
-        detailsDiv = document.createElement("div"); 
-        centerDiv = document.createElement("div");
-        let imgElement = document.createElement("img");
-        let titleH4 = document.createElement("H4");
-        let authorP = document.createElement("p");
-
-        cardDiv.className = "card";
-        cardDiv.id = tempId;
-        imageDiv.className = "image"
-        imgElement.src = tempImg;
-        detailsDiv.className = "details"
-        centerDiv.className = "center"
-        titleH4.className = "title"
-        titleH4.textContent = tempTitle
-        authorP.className = "author"
-        authorP.textContent = tempAuthor;
-
-        let node = document.getElementById("cards");
-        node.append(cardDiv);
-        cardDiv.append(imageDiv);
-        imageDiv.append(imgElement);
-        cardDiv.append(detailsDiv);
-        detailsDiv.append(centerDiv);
-        centerDiv.append(titleH4);
-        centerDiv.append(authorP);
+        addSingleBook(key);
 
     })
 }
+
+addBookToLibrary();
+
+function addSingleBook(key) {
+    let tempId = myLibrary[key].id;
+    let tempTitle = myLibrary[key].title;
+    let tempAuthor = myLibrary[key].author;
+    let tempImg = myLibrary[key].img;
+
+    let cardDiv = document.createElement("div"); 
+    imageDiv = document.createElement("div");
+    detailsDiv = document.createElement("div"); 
+    centerDiv = document.createElement("div");
+    let imgElement = document.createElement("img");
+    let titleH4 = document.createElement("H4");
+    let authorP = document.createElement("p");
+
+    cardDiv.className = "card";
+    cardDiv.id = tempId;
+    imageDiv.className = "image"
+    imgElement.src = tempImg;
+    detailsDiv.className = "details"
+    centerDiv.className = "center"
+    titleH4.className = "title"
+    titleH4.textContent = tempTitle
+    authorP.className = "author"
+    authorP.textContent = tempAuthor;
+
+    let node = document.getElementById("cards");
+    node.append(cardDiv);
+    cardDiv.append(imageDiv);
+    imageDiv.append(imgElement);
+    cardDiv.append(detailsDiv);
+    detailsDiv.append(centerDiv);
+    centerDiv.append(titleH4);
+    centerDiv.append(authorP);
+}
+
+// handle click of the add new book
+
+document.getElementById("plus").addEventListener('click', ()=> {
+    document.getElementById('new-form').style.display = "flex";
+}
+)
+
+let submitButton = document.getElementById("submitBtn")
+submitButton.addEventListener("click", function(event) {
+    document.getElementById('new-form').style.display = "none";
+    event.preventDefault();
+    let tempId = Number(currentId + 1)
+    let tempTitle = document.getElementById("author-form").value;
+    let tempAuthor = document.getElementById("title-form").value;
+    let tempImage = document.getElementById("image-form").value;
+    myLibrary[tempId] = new Book(tempId, tempTitle, tempAuthor, tempImage);
+    addSingleBook(tempId);
+    currentId = currentId++
+    document.getElementById("author-form").value = "";
+    document.getElementById("title-form").value = "";
+    document.getElementById("image-form").value = "";
+})
